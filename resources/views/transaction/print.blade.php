af32bb777ec4ce5d3151338845f5fd5f3b23668b
x-bal <mxbal026@gmail.com>
2024-06-10 14:25:01 +0700

    jquery

--- resources/views/transaction/print.blade.php
+++ resources/views/transaction/print.blade.php
@@ -78,7 +78,7 @@
     </div>
     @endforeach
 
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+    <script src="{{ asset('/js/jquery.min.js') }}"></script>
 
     <script>
         $(document).ready(function() {
